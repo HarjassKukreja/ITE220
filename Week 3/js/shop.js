a808746246ca0fefc5321709de092e501a9a4b0a
Harjass S. Kukreja <harjasskukreja9@gmail.com>
2016-12-02 13:19:15 +0700

    Week 3

---
+++ Week 3/js/shop.js
@@ -0,0 +1,10 @@
+var customerName = "Singh";
+var price = 10;
+var quantity = 2;
+var totalPrice = price * quantity;
+
+var customerElement= document.getElementById("customer-name");
+customerElement.textContent = customerName;
+
+var customerElement= document.getElementById("price");
+customerElement.textContent = ("$" + totalPrice);
