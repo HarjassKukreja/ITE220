d358723481bac14af8e19564ad0ac2c91f81189a
Harjass S. Kukreja <attahphol.kukreja@students.stamford.edu>
2017-01-19 11:11:36 +0700

    Update shop.js

--- Week4-2/js/shop.js
+++ Week4-2/js/shop.js
@@ -34,24 +34,56 @@
 
 
 var shop = {
-	customerName:"Singh",
-	totalPrice: 0,
-	products: ["Brooklyn T-Shirt White",
-     "Brooklyn T-Shirt Black",
-     "Apple Watch",
-     "Android Phone"],
+    customerName: "Singh",
+    totalPrice: 0,
+    products: [
+        "Brooklyn T-Shirt White",
+        "Brooklyn T-Shirt Black",
+        "Apple Watch",
+        "Android Phone"
+    ],
+    prices: [10, 10, 199, 159],
 
-    prices: [10 , 10 , 199 , 159],
-
+    // display customer name function
+    
     displayCustomerName: function() {
-    	var customerElement = document.getElementById("customer-name");
-    	customerElement.textContent = this.customerName;
-    }
+    var customerElement = document.getElementById("customer-name");
+    customerElement.textContent = this.customerName;
+    },
 
+    // display customer name function
+    displayCustomerList: function() {
+        var productsText = "";
+        var productsElement = document.getElementById("product-list");
+        
+        for ( var i = 0 ; i < shop.products.length; i++){
+    productsText += "<li class='list-group-item'><span class='badge'>$" + shop.prices[i] + "</span>" + shop.products[i] + "</li>";
+        };
+        
+    productsElement.innerHTML = productsText;
+    },
     
 
+    
+    // calculate total price function
+    
+    calculateTotalPrice: function() {
+        return (this.prices[0] + this.prices[1] + this.prices[2] + this.prices[3] ) * 0.75;
+    },
+    
+    // display total price function
+    
+    displayTotalPrice: function() {
+        this.totalPrice = this.calculateTotalPrice();
+        var totalPriceElement = document.getElementById("total-price");
+        totalPriceElement.textContent = this.calculateTotalPrice();
+    
+    }
 
-};
+}
 shop.displayCustomerName();
-shop.displaytotalprice();
-shop.displayproducts();
+shop.displayCustomerList();
+shop.displayTotalPrice();
+
+
+//Aj. some  taken in js.
